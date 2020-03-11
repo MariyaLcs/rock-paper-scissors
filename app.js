deb77f18fe1de8b6d0054f7b2d8d74f1efdacb28
Mariya Lucas <mariya.podosinova@gmail.com>
2020-03-11 10:32:58 +0000

    Added functions  main, game, getComputrChoice

--- app.js
+++ app.js
@@ -7,3 +7,27 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
+
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
+
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+}
+
+function main() {
+  rock_div.addEventListener("click", function() {
+    game("rock");
+  });
+  paper_div.addEventListener("click", function() {
+    game("paper");
+  });
+  scissors_div.addEventListener("click", function() {
+    game("scissors");
+  });
+}
+
+main();
