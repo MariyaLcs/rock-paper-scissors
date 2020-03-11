30ee7d67a36378607e0fc90e79b980b60ee89fb0
Mariya Lucas <mariya.podosinova@gmail.com>
2020-03-11 11:08:54 +0000

    Function win completed

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result>p");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
@@ -14,23 +14,31 @@ function getComputerChoice() {
   return choices[randomNumber];
 }
 
+function win(userChoice, computerChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  result_p.innerHTML = `${userChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}. You win!`;
+}
+function lose() {}
+function draw() {}
+
 function game(userChoice) {
   const computerChoice = getComputerChoice();
   switch (userChoice + computerChoice) {
     case "rockscissors":
     case "paperrock":
     case "scissorspaper":
-      console.log("USER WINS");
+      win(userChoice, computerChoice);
       break;
     case "rockpaper":
     case "paperscissors":
     case "scissorsrock":
-      console.log("Computer WINS");
+      lose(userChoice, computerChoice);
       break;
     case "rockrock":
     case "paperpaper":
     case "scissorsscissors":
-      console.log("Its a draw");
+      draw(userChoice, computerChoice);
       break;
   }
 }
